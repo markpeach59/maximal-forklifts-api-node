c2ea88881b68ecacc355a4c00c9e2bacf09af37f
Mark <mark@Marks-iMac.lan>
2023-03-10 12:42:18 +0000

    Seed Maxim Users

---
+++ seedMaxim.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const { User } = require("./models/user");
+
+const data = [
+  {
+     name: "Ian Capes",
+     email:"ian@maximgb.co.uk",
+     password:"$2b$10$2.OuxaWJdUYKhrEPWfCo.uyG4BC1w108CyEg/9YvY2UonisvQSjbe",
+     dealerId:ObjectId('640b21e500dd5f57b56efd54')
+     isMaximGB: true  
+  }
+
+];
+
+async function seed() {
+  await mongoose.connect(config.get("db"));
+
+/*
+  await User.deleteMany({}, function(err) {
+    if (err) console.log("Delete Many Failed");
+  });*/
+  
+  for (let item of data) {
+    const user = new User(item);
+    const doc = await user.save();
+    console.log(doc);
+  }
+  mongoose.disconnect();
+
+  console.info("Done!");
+}
+
+seed();
